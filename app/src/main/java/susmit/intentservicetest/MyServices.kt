76a9a387c0e81d363b155f38f62a69179e2dbfa4
susmit <susmitshegokar1@gmail.com>
2018-06-06 16:25:49 +0530

    here some bugs removed

--- app/src/main/java/susmit/intentservicetest/MyServices.kt
+++ app/src/main/java/susmit/intentservicetest/MyServices.kt
@@ -10,12 +10,12 @@ class MyServices: IntentService {
     constructor():super("Myservice")
 
     override fun onHandleIntent(intent: Intent?) {
-        var url =URL("https://lh3.googleusercontent.com/VwjJ6QXBv5dofFiyRUtZ02j98r80uw3G_t4R28RHyRfUzVnYQ4LUrQOXqQS7M7UYY3WR9Gc4hGmRyidGIvd2HPptOheYNf3qpq5751cwX_DI72YldFXLPpu29YvnvRuEyPSve3wbqVpQWCDhzaiZ6CVfc_uj_YFspGw_xnqfGeR6hbp0CsrpJhkp5SJyTy1skJ878B9ChqfGFpgvQrBKuPEhsOPGtKTSkGtmFQuqlzfvZqWfimgZQHCm99lY1IM0JPGeDcS1QCmK_b9YjzW5jrj13Hp63qzW9RW3iGUgVIp2wbmqAmS7MD_wqFQd0SkhnUFy_j7wSgqA65Yw8QmNpdF0iO7nzQI6Jh6WefxXumKlGK2JwOxDr11MwQ8xCol8u0eU948jsNYy0DFk_W8L6tXC9rGHealh3NtoVXXhGL7C-YOWFnOyw5IBfxLPaDMdUNfbPuvLfm8DZYQRAM3Qo2VUKqC8PSm5n5anUpAWXzLBlxK6vcHeBq2EnXm7ubIlz3t6xxVSYHcbiVtMkv3zooixK9r37G7TCByGYOMTP4n7xKgKdC6pwCb24Ml_g0wCxxgmvomEQxtfykwORVCqSpXdLow-fCINoG87xgA=w948-h712-no")
+        var url =URL("http://moseax.co.ke/wp-content/uploads/2017/04/android-800x445.png")
         var isr=url.openStream()
         var bmp=BitmapFactory.decodeStream(isr)
         var bmp1=ThumbnailUtils.extractThumbnail(bmp,300,300)
         var i=Intent()
-        i.setAction("Image_loading_done")
+        i.action = "Image_loading_done"
         i.putExtra("image",bmp1)
         sendBroadcast(i)
 
